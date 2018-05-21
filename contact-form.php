52aac2af0a990fd4f1a4de2db59ecf3b3eef6cdc
Kevin Michael <biobot.mike@gmail.com>
2018-05-21 13:53:39 +0200

    Update mailer with ajax and json response

--- contact-form.php
+++ contact-form.php
@@ -1,32 +1,27 @@
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
     <fieldset>
-        <label class="text-dark-blue" for="fullName">Full Name (required)</label>
+        <label class="text-dark-blue" for="fullName">Full Name (required)<span>*</span></label>
         <input class="input" type="text" name="fullName" value size="40" placeholder="Your full name.." required value="<?php echo $fullName; ?>"></input>
-        <span>* <?php if(isset($fullName_error)) echo $fullName_error; ?></span>
     </fieldset>
 
     <fieldset>
-        <label class="text-dark-blue" for="email">Email Address (required)</label>
+        <label class="text-dark-blue" for="email">Email Address (required)<span>*</span></label>
         <input class="input" type="email" name="email" value size="40" placeholder="Your email address.." required value="<?php echo $email; ?>"></input>
-        <span>* <?php if(isset($email_error)) echo $email_error; ?></span>
     </fieldset>
 
     <fieldset>
-        <label class="text-dark-blue" for="phoneNumber">Phone Number (required)</label>
+        <label class="text-dark-blue" for="phoneNumber">Phone Number</label>
         <input class="input" type="tel" name="phoneNumber" value size="14" placeholder="Your phone number.." required value="<?php echo $phoneNumber; ?>"></input>
-        <span>* <?php if(isset($phoneNumber_error)) echo $phoneNumber_error; ?></span>
     </fieldset>
 
     <fieldset>
-        <label class="text-dark-blue" for="subject">Subject (required)</label>
+        <label class="text-dark-blue" for="subject">Subject (required)<span>*</span></label>
         <input class="input" type="text" name="subject" value size="40" placeholder="Nature of enquiry.." required value="<?php echo $subject; ?>"></input>
-        <span>* <?php if(isset($subject_error)) echo $subject_error; ?></span>
     </fieldset>
 
     <fieldset>
-        <label class="text-dark-blue" for="message">Message (required)</label>
+        <label class="text-dark-blue" for="message">Message (required)<span>*</span></label>
         <textarea class="input" name="message" cols="40" rows="10" placeholder="Write your message.." required value="<?php echo $message; ?>"></textarea>
-        <span>* <?php if(isset($message_error)) echo $message_error; ?></span>
         <p class="hide">Leave this empty <input type="text" name="url"></input></p>
     </fieldset>
 
--- mailer.php
+++ mailer.php
@@ -1,44 +1,123 @@
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (isset($_POST['submit']) && isset($_POST['url']) && $_POST['url'] == '') {
-            $fullName = htmlspecialchars(stripslashes(trim($_POST['fullName'])));
-            $email = htmlspecialchars(stripslashes(trim($_POST['email'])));
-            $phoneNumber = htmlspecialchars(stripslashes(trim($_POST['phoneNumber'])));
-            $subject = htmlspecialchars(stripslashes(trim($_POST['subject'])));
-            $message = htmlspecialchars(stripslashes(trim($_POST['message'])));
+
+        //check if its an ajax request, exit if not
+        if (!isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && strtolower($_SERVER["HTTP_X_REQUESTED_WITH"]) != "xmlhttprequest") {
+            //exit script outputting json data
+            $output = json_encode(
+                array(
+                    'type' => 'error',
+                    'text' => 'Request is not a valid Ajax request.'
+                )
+            );
+            die($output);
+        }
+
+        //check $_POST vars are set, exit if any missing
+        if (!isset($_POST["fullName"]) || !isset($_POST["email"]) || !isset($_POST["subject"]) || !isset($_POST["message"]) ) {
+            $output = json_encode(
+                array(
+                    'type' => 'error',
+                    'text' => 'Highlighted input fields cannot be left blank!'
+                )
+            );
+            die($output);
+        }
+
+        if (isset($_POST["submit"]) && isset($_POST["url"]) && $_POST["url"] == '' && isset($_POST["fullName"]) && isset($_POST["email"]) && isset($_POST["subject"]) && isset($_POST["message"])) {
+            $fullName = htmlspecialchars(stripslashes(trim($_POST["fullName"])));
+            $email = htmlspecialchars(stripslashes(trim($_POST["email"])));
+            $phoneNumber = htmlspecialchars(stripslashes(trim($_POST["phoneNumber"])));
+            $subject = htmlspecialchars(stripslashes(trim($_POST["subject"])));
+            $message = htmlspecialchars(stripslashes(trim($_POST["message"])));
 
             $fullNamePattern = "/^[A-Za-z .'-]+$/";
             $emailPattern = "/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/";
             $contactNumberPattern = "/^(\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$/";
             $subjectPattern = "/^[A-Za-z .'-]+$/";
 
-            if (!preg_match($fullNamePattern, $fullName)) {
-                $fullName_error = 'Invalid name';
+            if (strlen($fullName) < 4) && (!preg_match($fullNamePattern, $fullName)) {
+                $output = json_encode(
+                    array(
+                        'type' => 'error',
+                        'text' => 'Name is too short.'
+                    )
+                );
+                die($output);
             }
             if (!preg_match($emailPattern, $email)) {
-                $email_error = 'Invalid email';
+                $output = json_encode(
+                    array(
+                        'type' => 'error',
+                        'text' => 'Please enter a valid email address.'
+                    )
+                );
+                die($output);
             }
-            if (!preg_match($contactNumberPattern, $phoneNumber)) {
-                $phoneNumber = 'Invalid contact number';
+            if (strlen($phoneNumber) < 11) && (!preg_match($contactNumberPattern, $phoneNumber)) {
+                $output = json_encode(
+                    array(
+                        'type' => 'error',
+                        'text' => 'Phone number is too short.'
+                    )
+                );
+                die($output);
             }
-            if (!preg_match($subjectPattern, $subject)) {
-                $subject_error = 'Invalid subject';
+            if (strlen($subject) < 4) && (!preg_match($subjectPattern, $subject)) {
+                $output = json_encode(
+                    array(
+                        'type' => 'error',
+                        'text' => 'Subject name is too short.'
+                    )
+                );
+                die($output);
             }
-            if (strlen($message) === 0) {
-                $message_error = 'Your message should not be empty';
+            if (strlen($message) === 0) && (strlen($message) < 5) {
+                $output = json_encode(
+                    array(
+                        'type' => 'error',
+                        'text' => 'Message is too short.'
+                    )
+                );
+                die($output);
             }
-        }
 
-        if (isset($_POST['submit']) && !isset($fullName_error) && !isset($email_error) && !isset($phoneNumber_error) && !isset($subject_error) && !isset($message_error) && isset($_POST['url']) && $_POST['url'] == '') {
             $to = "youremail@address.com"; // edit here
-            $from = "Your Website Contact Form"
-            $body = " Name: $fullName\n E-mail: $email\n Phone Number: $phoneNumber\n Message:\n $message";
+            $headers = "From: '.$fullName. '<'.$email.'>'.'" . "\r\n".
+                "Reply-To: '.$email.'" . "\r\n".
+                "X-Mailer: PHP/" . phpversion().
+                "MIME-Version: 1.0" . "\r\n".
+                "Content-Type: text/html; charset=UTF-8" . "\r\n";
+            $body = "<html>
+                <head>
+                    <title>Your Website Contact Form</title>
+                </head>
+                <body>
+                    <p><strong>From:</strong> $fullName</p>
+                    <p><strong>Email:</strong> $email</p>
+                    <p><strong>Phone:</strong> $phoneNumber</p>
+                    <p><strong>Subject:</strong> $subject</p>
+                    <p><strong>Message:</strong> $message</p>
+                </body>
+            </html>";
 
-            if (wp_mail($to, $from, $subject, $body)) {
-                header('Location: http://www.example.com/thankyou.php');
-                exit('Redirecting you to http://www.example.como/thankyou.php');
+            $sentMail = mail($to, $subject, $headers, $body);
+            if (!$sentMail) {
+                $output = json_encode(
+                    array(
+                        'type' => 'error',
+                        'text' => 'Could not send mail! Please contact administrator.'
+                    )
+                );
+                die($output);
             } else {
-                echo '<p>Error occurred, please try again later</p>';
+                $output = json_encode(
+                    array(
+                        'type' => 'success',
+                        'text' => '<p>Hi '.$name.'</p><p>Thank you for contacting us! We typically reply within 24 hours of receiveing your mail!</p><p>Please keep an eye on those mails.</p>'
+                    )
+                );
+                die($output);
             }
         }
     }
